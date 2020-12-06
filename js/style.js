1a61eda0f064a73d32ce3fbd709a44ff3614bf26
anik65 <www.hyper152@gmail.com>
2020-12-06 23:03:26 +0600

    portfolio

---
+++ js/style.js
@@ -0,0 +1,76 @@
+// $(document).ready(function(){
+//   $('.count').counterUp({
+//     delay: 10,
+//     time: 1100,
+// });
+//     $('.bxslider').bxSlider({ 
+//         controls:false, 
+//         pager:false, 
+//         auto:true,
+//         speed:800,
+//         mode:'vertical', 
+//       }); 
+//       AOS.init();
+     
+//       $('.carousel').carousel({
+//         interval: 1500
+//       });
+     
+//     });
+//      function animation(){
+//           document.querySelector(".slider").classList.add("animation");
+//      } 
+//        window.addEventListener("load",animation);
+
+//       document.querySelector(".nav-butt").addEventListener("click",function(){
+//         document.querySelector(".main-menu").classList.toggle("menu-show");
+        
+//       }); 
+//   function visible(){
+  
+//   var y=window.pageYOffset;
+//    x=document.querySelector("header").scrollHeight
+//   if(y>=x){
+//     document.querySelector(".main-menu").style.position="fixed";
+//  document.querySelector("nav").style.position="fixed";
+ 
+//    } 
+//     } 
+  
+//    window.addEventListener("scroll",visible);
+
+//  document.querySelector(".hyper") .addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// }); 
+// document.querySelector(".hyper1").addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// }); 
+// document.querySelector(".hyper2").addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// }); 
+// document.querySelector(".hyper3").addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// }); 
+// document.querySelector(".hyper4").addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// }); 
+// document.querySelector(".hyper5").addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// });
+// document.querySelector(".hyper6").addEventListener("click",function(){
+//   document.querySelector(".main-menu").classList.remove("menu-show");
+  
+// });  
+// $(document).ready(function(){
+//   $("#scroll").sticky({topSpacing:600});
+// });
+
+//  function myfunction(){
+//    document.querySelector("#load").style.display="none";
+//  }
